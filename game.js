b1fd8a28438318ccd0a0a6333395c8d00c360757
James Randolph <noman@James-iMac.local>
2019-05-28 14:14:01 -0400

    add gameConfig

--- game.js
+++ game.js
@@ -7,3 +7,16 @@ let gameOptions = {
 
     ballSpeedIncrease: 20
 }
+
+window.onload = function() {
+    let gameConfig = {
+        width: 480,
+        height: 640,
+        scene: playGame,
+        backgroundColor: 0x222222,
+        physics: {
+            default: "arcade"
+        }
+    }
+    game 
+}
